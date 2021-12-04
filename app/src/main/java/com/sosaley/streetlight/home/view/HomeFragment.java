3d97f98a6d337e46d4b0efab46817187d7283775
murali <murali.e@sosaley.in>
2021-12-04 15:14:12 +0530

    Menu Commit

--- app/src/main/java/com/sosaley/streetlight/home/view/HomeFragment.java
+++ app/src/main/java/com/sosaley/streetlight/home/view/HomeFragment.java
@@ -1,26 +1,38 @@
 package com.sosaley.streetlight.home.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.sosaley.streetlight.R;
+import com.sosaley.streetlight.databinding.FragmentHomeBinding;
 import com.sosaley.streetlight.home.modelview.HomeViewModel;
+import com.sosaley.streetlight.map.view.MapActivity;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        /*View root = inflater.inflate(R.layout.fragment_home, container, false);*/
+
+       FragmentHomeBinding fragmentHomeBinding =DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false);
+        View view=fragmentHomeBinding.getRoot();
+        fragmentHomeBinding.setHome(homeViewModel);
+
+        //activityLoginBinding= DataBindingUtil.setContentView(getActivity(),R.layout.fragment_home);
          //TextView textView = root.findViewById(R.id.text_home);
          //textView.setText("Murali");
         /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -29,6 +41,16 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
-        return root;
+
+        fragmentHomeBinding.mapImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/com/sosaley/streetlight/map/view/MapActivity.java
+++ app/src/main/java/com/sosaley/streetlight/map/view/MapActivity.java
@@ -1,6 +1,7 @@
 package com.sosaley.streetlight.map.view;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,17 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.sosaley.streetlight.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -26,34 +34,89 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     Double guindyLat=13.0067;
     Double guindyLon=80.2206;
 
+    Double velcherryLat=12.9815;
+    Double velcherryLon=80.2180;
+
+    LatLng latLng1=new LatLng(guindyLat,guindyLon);
+    LatLng latLng2=new LatLng(velcherryLat,velcherryLon);
+
+    List<LatLng>  coordinatesList=new ArrayList<>();
+
+
+    MarkerOptions markerOptions;
+    Marker marker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_mapfragment);
 
+        coordinatesList.add(latLng1);
+        coordinatesList.add(latLng2);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.streetLightMap);
         mapFragment.getMapAsync(this);
         mapView = mapFragment.getView();
 
-        addMarker(guindyLat, guindyLon);
+
+
+
 
         //fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapActivity.this);
 
     }
 
-    private void addMarker(Double guindyLat, Double guindyLon) {
-
-
-
-    }
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-         mGoogleMap=googleMap;
+        mGoogleMap=googleMap;
+
+        moveCamera(guindyLat, guindyLon);
+
+        for (int i = 0; i <coordinatesList.size() ; i++) {
+
+            addMarker(coordinatesList.get(i).latitude, coordinatesList.get(i).longitude);
+
+        }
+
+
+
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                moveCamera(guindyLat, guindyLon);
+            }
+        },1000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                addMarker(guindyLat, guindyLon);
+            }
+        },1000);*/
+
+
 
 
     }
+
+    private void moveCamera(Double guindyLat, Double guindyLon) {
+
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(guindyLat, guindyLon), 12));
+
+    }
+
+    private void addMarker(Double guindyLat, Double guindyLon) {
+
+        markerOptions = new MarkerOptions().position(new LatLng(guindyLat, guindyLon)).title("Murali");
+        marker = mGoogleMap.addMarker(markerOptions);
+        marker.showInfoWindow();
+
+
+
+    }
+
+
+
 }
